e8a397ae665f257cad71490490e34b14ebcbd56e
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2025-02-27 18:46:19 +0200

    Added methods for getting and adding products

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, BadRequestException } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateProductDto } from './dto/create-product.dto';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
+    constructor(private readonly productsService: ProductsService) { }
+
+    @Get()
+    @ApiOperation({ summary: 'Get all products' })
+    @ApiResponse({ status: 200, description: 'List of products' })
+    async getAllProducts() {
+        return this.productsService.getAllProducts();
+    }
+
+    @Post()
+    @ApiOperation({ summary: 'Add a new product' })
+    @ApiResponse({ status: 201, description: 'Product successfully created' })
+    @ApiResponse({ status: 400, description: 'Validation error' })
+    createProduct(
+        @Body() createProductDto: CreateProductDto) {
+        if (!createProductDto.name) {
+            throw new BadRequestException('Product name is required');
+        }
+
+        if (!createProductDto.calories) {
+            throw new BadRequestException('Product calories is required');
+        }
+
+        return this.productsService.createProduct(createProductDto.name, createProductDto.calories);
+    }
 }
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(@InjectRepository(Product)
+    private productRepository: Repository<Product>) { }
+
+    async getAllProducts(): Promise<Product[]> {
+        return this.productRepository.find();
+    }
+
+    async createProduct(name: string, calories: number) {
+        const product = this.productRepository.create({ name, calories });
+        return await this.productRepository.save(product);
+    }
+}
